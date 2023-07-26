5c365c6504f12976ba4d65094118579ebfa0c8b1
z-rayen <rayenzaghdoudi6@gmail.com>
2023-07-26 16:18:12 +0100

    update f_scraping.py

--- facebook_scraping/f_scraping.py
+++ facebook_scraping/f_scraping.py
@@ -1,7 +1,13 @@
 import facebook_scraper as fs
 import insert as ins
-pg_id=1
-for post in fs.get_posts('mosaiquefm',  cookies='C:/Users/rayen/Desktop/project/www.facebook.com_cookies.txt',options={"comments":True, "reactors": True}):
-    ins.ajout_post(post,pg_id)
-    ins.ajout_reaction(post)
-    ins.ajout_comments(post)
+import conne
+db_connection=conne.connection()
+cursor = db_connection.cursor()
+cursor.execute("SELECT * FROM pages")
+pages = cursor.fetchall()
+cursor.close()
+for page in pages:
+    for post in fs.get_posts(page[1],  cookies='C:/Users/rayen/Desktop/project/www.facebook.com_cookies.txt',options={"comments":True, "reactors": True}):
+        ins.ajout_post(post,page[0])
+        ins.ajout_reaction(post)
+        ins.ajout_comments(post)
