dc77646cab5bc1e44d1f574612fe3e936fbe049f
z-rayen <rayenzaghdoudi6@gmail.com>
2023-08-22 14:44:57 +0100

    update file 'flask_api.py'

--- facebook_scraping/f_scraping.py
+++ facebook_scraping/f_scraping.py
@@ -7,7 +7,7 @@ cursor.execute("SELECT * FROM pages")
 pages = cursor.fetchall()
 cursor.close()
 for page in pages:
-    for post in fs.get_posts(page[1],  cookies='C:/Users/rayen/Desktop/project/www.facebook.com_cookies.txt',options={"comments":True, "reactors": True}):
+    for post in fs.get_posts(page[1],  cookies='C:/Users/rayen/Desktop/project/www.facebook.com_cookies.json',options={"comments":True, "reactors": True}):
         ins.ajout_post(post,page[0])
         ins.ajout_reaction(post)
         ins.ajout_comments(post)
